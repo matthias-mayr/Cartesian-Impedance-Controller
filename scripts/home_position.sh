a263615fedd2b5228dbbe659cb868e3a0487e93f
Oussame Chouman <ou7028ch-s@student.lu.se>
2021-05-24 16:41:05 +0200

    Config: script stiffness values of home pos

--- scripts/home_position.sh
+++ scripts/home_position.sh
@@ -4,9 +4,7 @@ echo "Returning to the \"home\" position";
 
 
 ./change_goal.sh -0.5 -0.5 1 1 0 0 0  
-./change_stiffness.sh 200 200 200 100 100 100 50
-
-
+./change_stiffness.sh 50 50 50 30 30 30 20
 
 
 
