e1d0d5b59add662131153757c90360a41f8d8e49
Oussama Chouman <ou7028ch-s@student.lu.se>
2021-05-27 11:48:40 +0200

    Feature: script: Apply wrench torques

--- scripts/motion8.sh
+++ scripts/motion8.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#Apply FORCES
+
+#Apply TORQUES
 #------------
+
 # Stiffness configuration #1
-./change_stiffness.sh 100 100 10 50 50 50 0
+./change_stiffness.sh 200 200 200 5 50 50 0
 
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 0 7.5 0 0
 
 sleep 5
 
@@ -16,11 +18,11 @@ sleep 5
 sleep 5
 
 # Stiffness configuration #2
-./change_stiffness.sh 100 100 50 50 50 50 0
+./change_stiffness.sh 200 200 200 10 50 50 0
 
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 0 7.5 0 0
 
 sleep 5
 
@@ -28,10 +30,10 @@ sleep 5
 
 
 # Stiffness configuration #3
-./change_stiffness.sh 100 100 100 50 50 50 0
+./change_stiffness.sh 200 200 200 20 50 50 0
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 0 7.5 0 0
 
 sleep 5
 
@@ -40,7 +42,7 @@ sleep 5
 
 
 # Stiffness configuration #4
-./change_stiffness.sh 100 100 200 50 50 50 0
+./change_stiffness.sh 200 200 200 50 50 50 0
 sleep 2
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
@@ -52,10 +54,10 @@ sleep 5
 
 
 # Stiffness configuration #5
-./change_stiffness.sh 100 100 400 50 50 50 0
+./change_stiffness.sh 200 200 200 100 50 50 0
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 0 7.5 0 0
 
 sleep 5
 
@@ -64,12 +66,13 @@ sleep 5
 
 
 # Stiffness configuration #6
-./change_stiffness.sh 100 100 800 50 50 50 0
+./change_stiffness.sh 200 200 200 100 50 50 0
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 0 7.5 0 0
 
 sleep 5
 
 ./apply_wrench.sh 0 0 0 0 0 0
+
 
