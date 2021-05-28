b4d3efbb88d0980ede8476104a7cfa9d21030526
Oussame Chouman <ou7028ch-s@student.lu.se>
2021-05-28 17:58:12 +0200

    Config: sleep times

--- scripts/motion7.sh
+++ scripts/motion7.sh
@@ -2,74 +2,43 @@
 
 #Apply FORCES
 #------------
+force=5
+sleep_time_after_apply=30
 # Stiffness configuration #1
-./change_stiffness.sh 100 100 10 50 50 50 0
+./change_stiffness.sh 100 100 50 50 50 50 5
 
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 $force 0 0 0
 
-sleep 5
+sleep $sleep_time_after_apply
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
 sleep 5
+
+
 
 # Stiffness configuration #2
-./change_stiffness.sh 100 100 50 50 50 50 0
-
+./change_stiffness.sh 100 100 200 50 50 50 5
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 $force 0 0 0
 
-sleep 5
+sleep $sleep_time_after_apply
 
 ./apply_wrench.sh 0 0 0 0 0 0
+
+
 
 
 # Stiffness configuration #3
-./change_stiffness.sh 100 100 100 50 50 50 0
+./change_stiffness.sh 100 100 800 50 50 50 5
 sleep 2
 
-./apply_wrench.sh 0 0 7.5 0 0 0
+./apply_wrench.sh 0 0 $force 0 0 0
 
-sleep 5
-
-./apply_wrench.sh 0 0 0 0 0 0
-
-
-
-# Stiffness configuration #4
-./change_stiffness.sh 100 100 200 50 50 50 0
-sleep 2
-
-./apply_wrench.sh 0 0 7.5 0 0 0
-
-sleep 5
-
-./apply_wrench.sh 0 0 0 0 0 0
-
-
-
-# Stiffness configuration #5
-./change_stiffness.sh 100 100 400 50 50 50 0
-sleep 2
-
-./apply_wrench.sh 0 0 7.5 0 0 0
-
-sleep 5
-
-./apply_wrench.sh 0 0 0 0 0 0
-
-
-
-# Stiffness configuration #6
-./change_stiffness.sh 100 100 800 50 50 50 0
-sleep 2
-
-./apply_wrench.sh 0 0 7.5 0 0 0
-
-sleep 5
+sleep $sleep_time_after_apply
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
