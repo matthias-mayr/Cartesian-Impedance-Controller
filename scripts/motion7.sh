90ddeadbd16efd86ade416c2e363f4e48d43876b
Oussama Chouman <ou7028ch-s@student.lu.se>
2021-05-28 09:35:21 +0200

    Fix: script: More sleeping time between changes

--- scripts/motion7.sh
+++ scripts/motion7.sh
@@ -5,71 +5,63 @@
 # Stiffness configuration #1
 ./change_stiffness.sh 100 100 10 50 50 50 0
 
-sleep 2
+sleep 5
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
 sleep 5
+
 
 # Stiffness configuration #2
-./change_stiffness.sh 100 100 50 50 50 50 0
-
-sleep 2
+./change_stiffness.sh 100 100 100 50 50 50 0
+sleep 5
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
+sleep 5
 
 # Stiffness configuration #3
-./change_stiffness.sh 100 100 100 50 50 50 0
-sleep 2
+./change_stiffness.sh 100 100 200 50 50 50 0
+sleep 5
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
-
+sleep 5
 
 # Stiffness configuration #4
-./change_stiffness.sh 100 100 200 50 50 50 0
-sleep 2
+./change_stiffness.sh 100 100 400 50 50 50 0
+sleep 5
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
-
+sleep 5
 
 # Stiffness configuration #5
-./change_stiffness.sh 100 100 400 50 50 50 0
-sleep 2
-
-./apply_wrench.sh 0 0 7.5 0 0 0
-
-sleep 5
-
-./apply_wrench.sh 0 0 0 0 0 0
-
-
-
-# Stiffness configuration #6
 ./change_stiffness.sh 100 100 800 50 50 50 0
-sleep 2
+sleep 5
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
+
+
+
 
--- scripts/motion8.sh
+++ scripts/motion8.sh
@@ -1,35 +1,27 @@
 #!/bin/bash
 
 
+
+
 #Apply TORQUES
 #------------
 
 # Stiffness configuration #1
 ./change_stiffness.sh 200 200 200 5 50 50 0
 
-sleep 2
+sleep 5
 
 ./apply_wrench.sh 0 0 0 7.5 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
 sleep 5
+
+
 
 # Stiffness configuration #2
-./change_stiffness.sh 200 200 200 10 50 50 0
-
-sleep 2
-
-./apply_wrench.sh 0 0 0 7.5 0 0
-
-sleep 5
-
-./apply_wrench.sh 0 0 0 0 0 0
-
-
-# Stiffness configuration #3
 ./change_stiffness.sh 200 200 200 20 50 50 0
 sleep 2
 
@@ -39,40 +31,43 @@ sleep 5
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
+sleep 5
 
 
-# Stiffness configuration #4
+# Stiffness configuration #3
 ./change_stiffness.sh 200 200 200 50 50 50 0
-sleep 2
+sleep 5
 
 ./apply_wrench.sh 0 0 7.5 0 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
+sleep 5
 
+
+# Stiffness configuration #4
+./change_stiffness.sh 200 200 200 100 50 50 0
+sleep 5
+
+./apply_wrench.sh 0 0 0 7.5 0 0
+
+sleep 8
+
+./apply_wrench.sh 0 0 0 0 0 0
+
+sleep 5
 
 # Stiffness configuration #5
 ./change_stiffness.sh 200 200 200 100 50 50 0
-sleep 2
+sleep 5
 
 ./apply_wrench.sh 0 0 0 7.5 0 0
 
-sleep 5
+sleep 8
 
 ./apply_wrench.sh 0 0 0 0 0 0
 
-
-
-# Stiffness configuration #6
-./change_stiffness.sh 200 200 200 100 50 50 0
-sleep 2
-
-./apply_wrench.sh 0 0 0 7.5 0 0
-
 sleep 5
-
-./apply_wrench.sh 0 0 0 0 0 0
-
 
