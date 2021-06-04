d858221ece561245525e3bd4adcd62356fda0a03
Oussama Chouman <ou7028ch-s@student.lu.se>
2021-06-04 08:20:16 +0200

    Chore: cleans old motions, adds readme

--- scripts/motion3.sh
+++ scripts/motion3.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
 
-./change_stiffness.sh 1000 1000 1000 300 300 300 0
-
-sleep 1
-./change_goal.sh -0.5 0.0 1 1.0 0.0 0.0 0.0
-
-sleep 20
-
-./change_goal.sh -0.5 0.0 1 0.707 0.0 0.0 -0.707
-
-sleep 20
-
-./change_goal.sh -0.45 -0.5 1.1 1.0 0.0 0.0 0.0
-
-sleep 20
-./home_position.sh
-
-echo "Motion complete"
-
---
+++ scripts/readme_scripts.md
@@ -0,0 +1,22 @@
+# Available scripts
+
+## motion1
+Sliding motion
+
+## motion2
+
+## motion3
+
+## motion4
+
+## motion5
+
+## motion6
+Longer motion. goes back and forth 3 times with different stiffnesses.
+
+## motion7
+Wrenches for different stiffnessess. 
+
+## motion8
+
+Pose change + wrench.
