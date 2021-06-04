bb076504fd2ce337f89c73391989f96b6109f958
Oussame Chouman <ou7028ch-s@student.lu.se>
2021-06-04 16:43:23 +0200

    Chore: Renamed file name + new sliding file

--- scripts/sliding_mot2.sh
+++ scripts/sliding_mot2.sh
@@ -6,20 +6,20 @@
 ./home_position_sliding.sh
 echo "Press any key to start"
 read a
-force=5
+force=10
 time_simulation=200
-./log_data.sh $time_simulation sliding_motion_force$force &(
+./log_data.sh $time_simulation sliding_motion2_force$force &(
 
-./change_stiffness.sh 1000 1000 1 100 100 100 0
-python python_scripts/wait_for_stiffness.py 500 500 5 100 100 100 0
-
-
+./change_stiffness.sh 50 1000 1 200 200 1 0
+python python_scripts/wait_for_stiffness.py 1000 1000 1 200 200 1 0
+sleep 5
 ./apply_wrench.sh 0 0 $force 0 0 0
 python python_scripts/wait_for_wrench.py 0 0 $force 0 0 0
 python python_scripts/wait_for_not_moving.py
 
-
-./change_goal.sh -0.533 0.2 0.71 1 0 0 0
+x=-0.531824529171
+y=-0.421
+./change_goal.sh -0.7 $y 0.71 1 0 0 0
 sleep 2
 python python_scripts/wait_for_not_moving.py
 
