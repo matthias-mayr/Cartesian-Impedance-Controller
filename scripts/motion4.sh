fa91ac8e4f2d2f8f285d8553f22155ec10c2a018
Oussame Chouman <ou7028ch-s@student.lu.se>
2021-06-07 16:29:05 +0200

    Script: new interaction experiment manually extend the robot into singularity, low stiffness

--- scripts/motion4.sh
+++ scripts/motion4.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+
+#Singularity stuff
+
+./change_stiffness.sh 1 1 1 1 1 1 0
+
+python python_scripts/wait_for_stiffness.py 1 1 1 1 1 1 0
+
+echo "You can start pertubate the robot now"
+
+sleep 60
