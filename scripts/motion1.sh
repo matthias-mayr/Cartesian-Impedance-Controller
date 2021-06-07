dd897dbdf5317afde28d712e91f0dccbfc8fa35d
Oussama Chouman <ou7028ch-s@student.lu.se>
2021-06-07 10:16:35 +0200

    New: script experiment

--- scripts/motion1.sh
+++ scripts/motion1.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-#Sliding motion
-
-./home_position_sliding.sh
-read a
-echo "Press ready to start"
-./apply_wrench.sh 0 0 5 0 0 0
-python python_scripts/wait_for_wrench.py 0 0 5 0 0 0
+#testing singularity
 
 
+x=-0.901075541973
+y=-0.176262825727
+z=0.865163743496
 
-#Table : z: 0.707
+./change_stiffness.sh 80 80 80 30 30 30 0
+python python_scripts/wait_for_stiffness.py 80 80 80 30 30 30 0
+
+./change_goal.sh $x $y $z 1 0 0 0
+python python_scripts/wait_for_not_moving.py
+
+./home_position.sh
+
+
+./change_stiffness.sh 200 200 200 50 50 50 0
+python python_scripts/wait_for_stiffness.py 200 200 200 100 100 100 0
+
+./change_goal.sh $x $y $z 1 0 0 0
+python python_scripts/wait_for_not_moving.py
+
+
+./home_position.sh
+
+
+./change_stiffness.sh 500 500 500 100 100 100 0
+python python_scripts/wait_for_stiffness.py 500 500 500 100 100 100 0
+
+./change_goal.sh $x $y $z 1 0 0 0
+python python_scripts/wait_for_not_moving.py
+
