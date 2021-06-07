c9b92badf449e8418c6a03a49d214040d1a1dc91
Oussama Chouman <ou7028ch-s@student.lu.se>
2021-06-07 10:16:08 +0200

    Fix: script: wait for robot to stand still when returning to the home position, the code waits for the robot to stay still now

--- scripts/home_position.sh
+++ scripts/home_position.sh
@@ -4,6 +4,7 @@ echo "Returning to the \"home\" position";
 ./change_goal.sh -0.5 -0.5 1 1 0 0 0 & ./change_stiffness.sh 50 50 50 30 30 30 20
 
 python python_scripts/wait_for_stiffness.py 50 50 50 30 30 30 20
+python python_scripts/wait_for_not_moving.py
 
 ./change_stiffness.sh 200 200 200 100 100 100 0
 
