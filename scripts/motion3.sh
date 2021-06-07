7498a23f35a0f58ffd63de7b468008cb21613c29
Oussame Chouman <ou7028ch-s@student.lu.se>
2021-06-07 16:27:47 +0200

    Script: new singularity experiment Goes to a singularity, the out of the singularity

--- scripts/motion3.sh
+++ scripts/motion3.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#Singularity stuff
 
+./singularity_config1.sh
+
+./change_stiffness.sh 200 200 200 100 100 100 0
+
+python python_scripts/wait_for_stiffness.py 200 200 200 100 100 100 0
+
+
+./change_goal.sh -0.495254188776 0.427890121937 1.39696657658 0.50154197216 -0.416139572859 -0.265110373497 -0.710633516312
+
+python python_scripts/wait_for_not_moving.py
