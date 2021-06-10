30b9cf000e34a6529ae7c9c110e1fc6ecf2853fd
Oussama Chouman <ou7028ch-s@student.lu.se>
2021-06-10 22:32:14 +0200

    New: motion for low stiffness trajectories

--- scripts/motion9.sh
+++ scripts/motion9.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-sleep 60
+./change_stiffness.sh 150 150 150 50 50 50 0
 
+python python_scripts/wait_for_stiffness.py 150 150 150 50 50 50 0
 
+./change_goal.sh -0.5 0 1.0 1 0 0 0
+
+python python_scripts/wait_for_not_moving.py 
+
+sleep 5
+
+./change_goal.sh -0.5 -0.5 1.0 1 0 0 0
+
+python python_scripts/wait_for_not_moving.py 
+
+sleep 5
+
+./change_stiffness.sh 100 100 100 50 50 50 0
+
+python python_scripts/wait_for_stiffness.py 100 100 100 50 50 50 0
+
+./change_goal.sh -0.5 0 1.0 1 0 0 0
+
+python python_scripts/wait_for_not_moving.py 
+
+sleep 5
+
+python python_scripts/wait_for_not_moving.py 
+
+./change_goal.sh -0.5 -0.5 1.0 1 0 0 0
+
+python python_scripts/wait_for_not_moving.py 
+
+sleep 5
