ce4fb215163c030b91134b3480936b3491506669
Oussame Chouman <ou7028ch-s@student.lu.se>
2021-06-11 15:26:56 +0200

    Config: stiffness values

--- scripts/motion9.sh
+++ scripts/motion9.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-./change_stiffness.sh 150 150 150 50 50 50 0
 
-python python_scripts/wait_for_stiffness.py 150 150 150 50 50 50 0
+./change_stiffness.sh 300 300 300 50 50 50 0
+
+python python_scripts/wait_for_stiffness.py 300 300 300 50 50 50 0
 
 ./change_goal.sh -0.5 0 1.0 1 0 0 0
 
 python python_scripts/wait_for_not_moving.py 
 
-sleep 5
 
-./change_goal.sh -0.5 -0.5 1.0 1 0 0 0
+
+./change_goal.sh -0.5 -0.5 1.0 1 0 0 0 & ./change_stiffness.sh 300 300 300 50 50 50 5
 
 python python_scripts/wait_for_not_moving.py 
 
-sleep 5
+
+
+
+./change_stiffness.sh 200 200 200 50 50 50 0
+
+python python_scripts/wait_for_stiffness.py 200 200 200 50 50 50 0
+
+./change_goal.sh -0.5 0 1.0 1 0 0 0
+
+python python_scripts/wait_for_not_moving.py 
+
+
+
+./change_goal.sh -0.5 -0.5 1.0 1 0 0 0 & ./change_stiffness.sh 200 200 200 50 50 50 5
+
+python python_scripts/wait_for_not_moving.py 
+
+
+
+
+
+
 
 ./change_stiffness.sh 100 100 100 50 50 50 0
 
@@ -28,8 +50,14 @@ sleep 5
 
 python python_scripts/wait_for_not_moving.py 
 
-./change_goal.sh -0.5 -0.5 1.0 1 0 0 0
+./change_goal.sh -0.5 -0.5 1.0 1 0 0 0 & ./change_stiffness.sh 100 100 100 50 50 50 5
 
 python python_scripts/wait_for_not_moving.py 
 
 sleep 5
+
+
+
+
+
+
