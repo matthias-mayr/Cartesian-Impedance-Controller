7a4c76ae548fa6b28472808aead3183645ce0c58
Matthias Mayr <matthias.mayr@cs.lth.se>
2022-11-04 17:16:38 +0100

    Update: Move to upstream RBDyn & no SIMD
    - Use of upstream RBDyn, SpaceVecAlg & mc_rbdyn_urdf
    - Disabled SIMD compile option
    - Sets "RBDyn" and "mc_rbdyn_urdf" as target link libraries

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -13,30 +13,30 @@ fi
 cd depends
 
 # SpaceVecAlg
-git clone --recursive https://github.com/costashatz/SpaceVecAlg.git
+git clone --recursive https://github.com/jrl-umi3218/SpaceVecAlg
 cd SpaceVecAlg
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SIMD=ON -DPYTHON_BINDING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_BINDING=OFF ..
 make -j
 sudo make install
 
 cd ../..
 
 # RBDyn
-git clone --recursive https://github.com/costashatz/RBDyn.git
+git clone --recursive https://github.com/jrl-umi3218/RBDyn
 cd RBDyn
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SIMD=ON -DPYTHON_BINDING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_BINDING=OFF ..
 make -j
 sudo make install
 
 cd ../..
 
 # mc_rbdyn_urdf
-git clone --recursive https://github.com/costashatz/mc_rbdyn_urdf.git
+git clone --recursive https://github.com/jrl-umi3218/mc_rbdyn_urdf
 cd mc_rbdyn_urdf
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_SIMD=ON -DPYTHON_BINDING=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_BINDING=OFF ..
 make -j
 sudo make install
 
