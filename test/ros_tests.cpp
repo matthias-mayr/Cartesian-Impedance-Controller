50b9e3423d50de9079dcc617dca46de2f17cefc4
Matthias Mayr <matthias.mayr@cs.lth.se>
2023-10-31 19:49:23 +0000

    New: Clean up ROS tests, switch to example robot
    - Minor modifications to the CMakeLists
    - Bumps package version to 1.1
    - Switches to the example controller configuration

--- test/ros_tests.cpp
+++ test/ros_tests.cpp
@@ -6,15 +6,16 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <dynamic_reconfigure/Config.h>
 
+const std::string ctrl_name = "/CartesianImpedance_trajectory_controller";
 
 //ROSBaseTests tested that key topics in the library are being written.
 TEST(ROSBaseTests, referenceposeTests)
 {
-    ros::NodeHandle n;
-    ros::Duration timeout(10);
+  ros::NodeHandle n;
+  ros::Duration timeout(1);
   for (int i = 0; i < 5; i++) {
     auto msg_pose = ros::topic::waitForMessage<geometry_msgs::PoseStamped>(
-        "/CartesianImpedance_trajectory_controller/reference_pose", n, timeout);
+        ctrl_name + "/reference_pose", n, timeout);
 
     ASSERT_TRUE(msg_pose != nullptr) << "Did not receive message on iteration " << i + 1;
 
@@ -25,14 +26,13 @@ TEST(ROSBaseTests, referenceposeTests)
 
 TEST(ROSBaseTests, commandedtorqueTests)
 {
-    ros::NodeHandle n;
-    ros::Duration timeout(5);
+  ros::NodeHandle n;
+  ros::Duration timeout(5);
+  const std::string torque_topic {ctrl_name + "/commanded_torques"};
   for (int i = 0; i < 5; i++) {
-    auto msg_torque = ros::topic::waitForMessage<std_msgs::Float64MultiArray>(
-        "/CartesianImpedance_trajectory_controller/commanded_torques", n, timeout);
+    auto msg_torque = ros::topic::waitForMessage<std_msgs::Float64MultiArray>(torque_topic, n, timeout);
 
     ASSERT_TRUE(msg_torque != nullptr) << "Did not receive message on iteration " << i + 1;
-
     EXPECT_LE(std::abs(msg_torque->data[0]), 1000.0);
   }
 
@@ -40,11 +40,11 @@ TEST(ROSBaseTests, commandedtorqueTests)
 
 //ROSReconfigureTests tested that we are able to modify external wrench, virtual stiffness, and damping online.
 TEST(ROSReconfigureTests, wrenchTest) {
-    ros::NodeHandle n;
-    ros::Duration timeout(10);
+  ros::NodeHandle n;
+  ros::Duration timeout(10);
   for (int i = 0; i < 5; i++) {
     auto msg_wup = ros::topic::waitForMessage<dynamic_reconfigure::Config>(
-        "/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/parameter_updates", n, timeout);
+        ctrl_name + "/cartesian_wrench_reconfigure/parameter_updates", n, timeout);
 
     ASSERT_TRUE(msg_wup != nullptr) << "Did not receive message on iteration " << i + 1;
 
@@ -58,11 +58,11 @@ TEST(ROSReconfigureTests, wrenchTest) {
 }
 
 TEST(ROSReconfigureTests, stiffnessTest) {
-    ros::NodeHandle n;
-    ros::Duration timeout(5);
+  ros::NodeHandle n;
+  ros::Duration timeout(5);
   for (int i = 0; i < 5; i++) {
     auto msg_stup = ros::topic::waitForMessage<dynamic_reconfigure::Config>(
-        "/CartesianImpedance_trajectory_controller/stiffness_reconfigure/parameter_updates", n, timeout);
+        ctrl_name + "/stiffness_reconfigure/parameter_updates", n, timeout);
 
     ASSERT_TRUE(msg_stup != nullptr) << "Did not receive message on iteration " << i + 1;
 
@@ -80,7 +80,7 @@ TEST(ROSReconfigureTests, dampingTest) {
     ros::Duration timeout(5);
   for (int i = 0; i < 5; i++) {
     auto msg_dup = ros::topic::waitForMessage<dynamic_reconfigure::Config>(
-        "/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/parameter_updates", n, timeout);
+        ctrl_name + "/damping_factors_reconfigure/parameter_updates", n, timeout);
 
     ASSERT_TRUE(msg_dup != nullptr) << "Did not receive message on iteration " << i + 1;
 
@@ -95,53 +95,50 @@ TEST(ROSReconfigureTests, dampingTest) {
 
 //ROSParameterTests make sure that all the necessary ROS parameters have been created.
 TEST(ROSParameterTests, paramTest) {
-    ros::NodeHandle n;
-    ros::Duration timeout(5);
-    std::vector<std::string> params_to_check = {
-	"/CartesianImpedance_trajectory_controller/arm_id",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/apply_wrench",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/f_x",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/f_y",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/f_z",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/tau_x",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/tau_y",
-	"/CartesianImpedance_trajectory_controller/cartesian_wrench_reconfigure/tau_z",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/nullspace_damping",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/rotation_x",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/rotation_y",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/rotation_z",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/translation_x",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/translation_y",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/translation_z",
-	"/CartesianImpedance_trajectory_controller/damping_factors_reconfigure/update_damping_factors",
-	"/CartesianImpedance_trajectory_controller/delta_tau_max",
-	"/CartesianImpedance_trajectory_controller/dynamic_reconfigure",
-	"/CartesianImpedance_trajectory_controller/end_effector",
-	"/CartesianImpedance_trajectory_controller/filtering/nullspace_config",
-	"/CartesianImpedance_trajectory_controller/filtering/pose",
-	"/CartesianImpedance_trajectory_controller/filtering/stiffness",
-	"/CartesianImpedance_trajectory_controller/filtering/wrench",
-	"/CartesianImpedance_trajectory_controller/handle_trajectories",
-	"/CartesianImpedance_trajectory_controller/joints",
-	"/CartesianImpedance_trajectory_controller/root_frame",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/nullspace_stiffness",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/rotation_x",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/rotation_y",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/rotation_z",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/translation_x",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/translation_y",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/translation_z",
-	"/CartesianImpedance_trajectory_controller/stiffness_reconfigure/update_stiffness",
-	"/CartesianImpedance_trajectory_controller/type",
-	"/CartesianImpedance_trajectory_controller/update_frequency",
-	"/CartesianImpedance_trajectory_controller/verbosity/state_msgs",
-	"/CartesianImpedance_trajectory_controller/verbosity/tf_frames",
-	"/CartesianImpedance_trajectory_controller/verbosity/verbose_print",
-	"/CartesianImpedance_trajectory_controller/wrench_ee_frame"
+  const std::vector<std::string> params_to_check = {
+    "cartesian_wrench_reconfigure/apply_wrench",
+    "cartesian_wrench_reconfigure/f_x",
+    "cartesian_wrench_reconfigure/f_y",
+    "cartesian_wrench_reconfigure/f_z",
+    "cartesian_wrench_reconfigure/tau_x",
+    "cartesian_wrench_reconfigure/tau_y",
+    "cartesian_wrench_reconfigure/tau_z",
+    "damping_factors_reconfigure/nullspace_damping",
+    "damping_factors_reconfigure/rotation_x",
+    "damping_factors_reconfigure/rotation_y",
+    "damping_factors_reconfigure/rotation_z",
+    "damping_factors_reconfigure/translation_x",
+    "damping_factors_reconfigure/translation_y",
+    "damping_factors_reconfigure/translation_z",
+    "damping_factors_reconfigure/update_damping_factors",
+    "delta_tau_max",
+    "dynamic_reconfigure",
+    "end_effector",
+    "filtering/nullspace_config",
+    "filtering/pose",
+    "filtering/stiffness",
+    "filtering/wrench",
+    "handle_trajectories",
+    "joints",
+    "root_frame",
+    "stiffness_reconfigure/nullspace_stiffness",
+    "stiffness_reconfigure/rotation_x",
+    "stiffness_reconfigure/rotation_y",
+    "stiffness_reconfigure/rotation_z",
+    "stiffness_reconfigure/translation_x",
+    "stiffness_reconfigure/translation_y",
+    "stiffness_reconfigure/translation_z",
+    "stiffness_reconfigure/update_stiffness",
+    "type",
+    "update_frequency",
+    "verbosity/state_msgs",
+    "verbosity/tf_frames",
+    "verbosity/verbose_print",
+    "wrench_ee_frame"
     };
 
     for (const auto &param : params_to_check) {
-        EXPECT_TRUE(ros::param::has(param)) << "Parameter [" << param << "] does NOT exist.";
+      EXPECT_TRUE(ros::param::has("/" + ctrl_name + "/" + param)) << "Parameter [" << param << "] does NOT exist.";
     }
 }
 
@@ -150,13 +147,3 @@ int main(int argc, char **argv){
   ros::init(argc, argv, "test_ros");
   return RUN_ALL_TESTS();
 }
-
-
-
-
-
-
-
-
-
-
